a8fcb4719a580968f1ff6554ff5997fd66713753
CubicPill <huazhengchang97@gmail.com>
2018-03-02 21:42:57 +0800

    add functions

--- webgui.py
+++ webgui.py
@@ -6,9 +6,19 @@ def index():
     pass
 
 
+def save_result():
+    pass
+
+
+def read_current():
+    pass
+
+
 def main():
     global app
     app = Flask(__name__)
+    print('Please go to http://localhost:2333 to view the web GUI')
+    app.run(host='localhost', port=2333)
 
 
 def init():
