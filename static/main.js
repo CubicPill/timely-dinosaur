34f5ad62f63a063ebf18f47c351eb1de3964b2eb
CubicPill <cubicpill@gmail.com>
2018-05-28 20:16:05 +0800

    functions

--- static/main.js
+++ static/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    initPage();
+});
+
+function initPage() {
     $("#query-btn").click(function () {
         console.log('search!');
         let input = $("#query-txt");
@@ -11,19 +15,34 @@ $(document).ready(function () {
             data: JSON.stringify({
                 query: input.val()
             }),
-            success: function (data) {
-                console.log(data);
-            },
-
+            success: onSearchSuccess,
             contentType: "application/json"
         });
         input.val("");
     });
-});
+    loadSavedResults();
+}
 
-function updateSearchResults(data) {
+function onSearchSuccess(data) {
     if (!data.ok) {
+        console.error('API return: Not OK');
         return;
     }
+    console.log(data);
+}
+
+function loadSavedResults() {
+    $.ajax({
+        type: "GET",
+        url: "/load",
+        success: function (data) {
+            console.log(data);
+        },
+
+        contentType: "application/json"
+    });
+}
+
+function showSearchResults(data) {
 
 }
