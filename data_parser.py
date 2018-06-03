dfad09cdbb557c8c5f12175342d10cdab5010bc6
CubicPill <cubicpill@gmail.com>
2018-06-03 22:09:04 +0800

    structure

--- data_parser.py
+++ data_parser.py
@@ -1,2 +1,19 @@
-def parse_single_course_data():
+def parse_single_course_data(data: dict) -> dict:
     pass
+
+
+def parse_batch_course_data(data: dict) -> dict:
+    pass
+
+
+class Course:
+    def __init__(self, name, course_no, **kwargs):
+        pass
+
+    @staticmethod
+    def from_json(data: str):
+        pass
+
+    @staticmethod
+    def from_dict(data: dict):
+        pass
