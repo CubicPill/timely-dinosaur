fbb6da1aa605309cc3dfb01e291d952e608b3b44
CubicPill <cubicpill@gmail.com>
2018-09-07 18:31:19 +0800

    model file

--- utils.py
+++ utils.py
@@ -11,6 +11,22 @@ MAIN_URL = 'http://jwxt.sustc.edu.cn/jsxsd/framework/xsMain.jsp'
 LOGIN_SERVER_ADDR = 'https://cas.sustc.edu.cn'
 _global_session = requests.session()
 
+ENROLL_URLS = [
+    'http://jwxt.sustc.edu.cn/jsxsd/xsxkkc/bxxkOper?jx0404id={id}&xkzy=&trjf=',
+    # 必修选课
+    'http://jwxt.sustc.edu.cn/jsxsd/xsxkkc/xxxkOper?jx0404id={id}&xkzy=&trjf=',
+    # 选修选课
+    'http://jwxt.sustc.edu.cn/jsxsd/xsxkkc/bxqjhxkOper?jx0404id={id}&xkzy=&trjf=',
+    # 本学期计划选课
+    'http://jwxt.sustc.edu.cn/jsxsd/xsxkkc/knjxkOper?jx0404id={id}&xkzy=&trjf=',
+    # 跨年级
+    'http://jwxt.sustc.edu.cn/jsxsd/xsxkkc/fawxkOper?jx0404id={id}&xkzy=&trjf=',
+    # 跨专业
+    'http://jwxt.sustc.edu.cn/jsxsd/xsxkkc/ggxxkxkOper?jx0404id={id}&xkzy=&trjf='
+    # 公选课
+]
+TYPES_STR = ['必修', '选修', '本学期计划', '跨年级', '跨专业', '公共课']
+
 
 class CourseType(IntEnum):
     REQUIRED = 0
@@ -101,23 +117,6 @@ def remove_session_pickle():
         os.remove('session.pickle')
         return True
     return False
-
-
-ENROLL_URLS = [
-    'http://jwxt.sustc.edu.cn/jsxsd/xsxkkc/bxxkOper?jx0404id={id}&xkzy=&trjf=',
-    # 必修选课
-    'http://jwxt.sustc.edu.cn/jsxsd/xsxkkc/xxxkOper?jx0404id={id}&xkzy=&trjf=',
-    # 选修选课
-    'http://jwxt.sustc.edu.cn/jsxsd/xsxkkc/bxqjhxkOper?jx0404id={id}&xkzy=&trjf=',
-    # 本学期计划选课
-    'http://jwxt.sustc.edu.cn/jsxsd/xsxkkc/knjxkOper?jx0404id={id}&xkzy=&trjf=',
-    # 跨年级
-    'http://jwxt.sustc.edu.cn/jsxsd/xsxkkc/fawxkOper?jx0404id={id}&xkzy=&trjf=',
-    # 跨专业
-    'http://jwxt.sustc.edu.cn/jsxsd/xsxkkc/ggxxkxkOper?jx0404id={id}&xkzy=&trjf='
-    # 公选课
-]
-TYPES_STR = ['必修', '选修', '本学期计划', '跨年级', '跨专业', '公共课']
 
 
 def load_config_from_file():
